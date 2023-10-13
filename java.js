fd878165e0aa110d5f76effa6a017cf69cdbfdb8
Abdel <112128079+DjeAbdel0@users.noreply.github.com>
2023-10-13 13:00:00 -0400

    update_destinations_DestinationBtn

--- java.js
+++ java.js
@@ -1,11 +1,9 @@
 const chapters = {
   contrat: {
     titre: "Proposition de contrat",
-    description:
-      "Alors que tu es en plein cours à l'école, un représentant du Real Madrid entre dans la salle. Tous les regards se tournent vers lui. Il s'approche de toi et te tends une lettre qui contient un contrat professionnel avec le club.",
+    description: "Alors que tu es en plein cours à l'école, un représentant du Real Madrid entre dans la salle. Tous les regards se tournent vers lui. Il s'approche de toi et te tends une lettre qui contient un contrat professionnel avec le club.",
     image: "./assets/img/contract.jpg",
-    boutons: [
-      {
+    boutons: [{
         titre: "Accepter le contrat",
         destination: "match",
       },
@@ -18,58 +16,46 @@ const chapters = {
   mcdo: {
     titre: "Echec",
     image: "./assets/img/stadium.png",
-    description:
-      "Malheureusement rien tu echoue lamentablement a lecole. Tu garde un regret eternel pour avoir refuser ce contrat",
-      boutons: [],
-    },
+    description: "Malheureusement rien tu echoue lamentablement a lecole. Tu garde un regret eternel pour avoir refuser ce contrat",
+    boutons: [],
+  },
   match: {
     titre: "Premier match",
-    description:
-      "Après avoir choisi de rejoindre le Real Madrid, tu te retrouves sur le banc des remplaçants lors de ton premier match. Le match est intense, mais l'entraîneur ne t'a pas encore fait entrer.",
+    description: "Après avoir choisi de rejoindre le Real Madrid, tu te retrouves sur le banc des remplaçants lors de ton premier match. Le match est intense, mais l'entraîneur ne t'a pas encore fait entrer.",
     image: "./assets/img/bench_starter.jpg",
-    boutons: [
-      {
+    boutons: [{
         titre: "Demander au coach de te faire rentrer sur le terrain.",
         destination: "confiance",
       },
       {
-        titre:
-          "Attendre patiemment que le coach décide de te faire entrer de lui-même.",
+        titre: "Attendre patiemment que le coach décide de te faire entrer de lui-même.",
         destination: "patience",
       },
     ],
   },
   confiance: {
     titre: "Gagner la confiance de tous",
-    description:
-      "Tu rentre a la 2eme mi-temps. Tu fais un bon match et tu commence a prendre tes aises. Tu es completement épuisé a la fin du match.",
+    description: "Tu rentre a la 2eme mi-temps. Tu fais un bon match et tu commence a prendre tes aises. Tu es completement épuisé a la fin du match.",
     image: "./assets/img/coach.jpg",
-    boutons: [
-      {
-        titre: "Travailler dur et viser la victoire.",
-        destination: "victoire",
-      },
-    ],
+    boutons: [{
+      titre: "Travailler dur et viser la victoire.",
+      destination: "victoire",
+    }, ],
   },
   patience: {
     titre: "Faire preuve de patience",
-    description:
-      "Tu as choisi d'attendre patiemment que le coach décide de te faire entrer. Il te fait rentrer a la 82eme minute. Tu reussis a marquer le but gagnant 2 mins apres. Bravo ",
+    description: "Tu as choisi d'attendre patiemment que le coach décide de te faire entrer. Il te fait rentrer a la 82eme minute. Tu reussis a marquer le but gagnant 2 mins apres. Bravo ",
     image: "./assets/img/bench.jpg",
-    boutons: [
-      {
-        titre: "Apres match",
-        destination: "victoire",
-      },
-    ],
+    boutons: [{
+      titre: "Apres match",
+      destination: "victoire",
+    }, ],
   },
   victoire: {
     titre: "La Victoire",
-    description:
-      "Félicitations, ton équipe a remporté le match ! Tu as contribué à la victoire de l'équipe avec 1 but, et maintenant tu as plusieurs choix à faire. Que vas-tu faire ensuite ?",
+    description: "Félicitations, ton équipe a remporté le match ! Tu as contribué à la victoire de l'équipe avec 1 but, et maintenant tu as plusieurs choix à faire. Que vas-tu faire ensuite ?",
     image: "./assets/img/victoire.jpg",
-    boutons: [
-      {
+    boutons: [{
         titre: "S'entraîner pour s'améliorer davantage.",
         destination: "entrainement",
       },
@@ -83,52 +69,55 @@ const chapters = {
       },
     ],
   },
-
   entrainement: {
     titre: "Entraînement Intensif",
     image: "./assets/img/stadium.png",
-    description:
-      "Pour devenir un meilleur joueur, tu décides de t'engager dans un programme d'entraînement intensif. Cela nécessite un dévouement total et des heures d'efforts sur le terrain. Le stade entier est impressionné par ton niveau de jeu.",
-      boutons: [],
+    description: "Pour devenir un meilleur joueur, tu décides de t'engager dans un programme d'entraînement intensif. Cela nécessite un dévouement total et des heures d'efforts sur le terrain. ",
+    boutons: [{
+      titre: "Prochain match",
+      destination: "matchSuiv",
+    }, ],
   },
   repos: {
     titre: "Un Repos Bien Mérité",
     image: "./assets/img/stadium.png",
-    description:
-      "Après l'effort, le réconfort. Tu prends du temps pour te reposer et récupérer de l'intensité du match. Le match suivant, tu as fait preuve d'un niveau correct, tu es presque le nouveau titulaire de ton poste.", 
-      boutons: [],
+    description: "Après l'effort, le réconfort. Tu prends du temps pour te reposer et récupérer de l'intensité du match. Go dormir il y a un autre match demain.",
+    boutons: [{
+      titre: "Prochain match",
+      destination: "matchSuiv",
+    }, ],
   },
   fete: {
     titre: "Fêter la Victoire",
     image: "./assets/img/stadium.png",
-    description:
-      "C'est l'heure de la fête ! Tu a célèbré la victoire toute la nuit. Le match suivant, tu était trop épuisé pour pouvoir montret un bon niveau de jeu. Le coach te remplace après 10 minutes de jeu. Ton parcours dans cette nouvelle équipe semble compromis.",
-      boutons: [],
+    description: "C'est l'heure de la fête ! Tu a célèbré la victoire toute la nuit. Go dormir il y a un autre match demain.",
+    boutons: [{
+      titre: "Prochain match",
+      destination: "matchSuiv",
+    }, ],
   },
   matchSuiv: {
     titre: "Grand moment",
-    
-    description:
-      "Un joueur vient de se blesser, le coach décide de te faire rentrer. Tu commence a t'échauffer",
+
+    description: "Un joueur vient de se blesser, le coach décide de te faire rentrer. Tu commence a t'échauffer",
     image: "./assets/img/bench.jpg",
-    boutons: [
-      {
-        titre: "remplacement",
-        destination: "remplacement",
-      },
-    ],
+    boutons: [{
+      titre: "remplacement",
+      destination: "contrat",
+    }, ],
   },
 };
 
 let titreChap = document.querySelectorAll(".titre");
 let textChap = document.querySelector(".text");
 let imageChap = document.getElementById("logo");
-let chapitreActuel = "contrat";
 
-goToChapter(chapitreActuel);
+
 
 let twist = 0;
+
 function goToChapter(chapitre) {
+
   let obj = chapters[chapitre];
   if (obj == undefined) {
     console.log("Clé de chapitre invalide : " + chapitre);
@@ -143,7 +132,7 @@ function goToChapter(chapitre) {
       while (boutons.firstChild) {
         boutons.removeChild(boutons.firstChild);
       }
-        //creer btn et ajouter le text
+      //creer btn et ajouter le text
       for (let i = 0; i < obj.boutons.length; i++) {
         let nouveauBtn = document.createElement('button');
         nouveauBtn.setAttribute("id", "btn");
@@ -157,4 +146,23 @@ function goToChapter(chapitre) {
     }
   }
 
+  if (chapitre === "victoire" && obj.boutons[i].titre === "entrainement") {
+    nouveauBtn.addEventListener('click', () => {
+      twist === 1;
+    })
+  } else if (chapitre === "victoire" && obj.boutons[i].titre === "repos") {
+    nouveauBtn.addEventListener('click', () => {
+      twist === 2;
+    })
+  } else if (chapitre === "victoire" && obj.boutons[i].titre === "fete") {
+    nouveauBtn.addEventListener('click', () => {
+      twist === 3;
+    })
+  }
+  if (twist === 1 && chapitre === "matchSuiv" && obj.boutons[i].titre === "remplacement" ) {
+    obj.boutons[i].destination == "fete";
+   
+  }
 }
+
+goToChapter("contrat");
